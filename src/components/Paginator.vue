96d6da568bdc96e3b2b41de00495052d866230b7
Osman <osman.dulger@foreks.com>
2022-03-12 17:03:48 +0300

    Fix: Move paginator through to the component

--- src/components/Paginator.vue
+++ src/components/Paginator.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
-    <div class="container">
-      <Product
-        class="product"
-        v-for="item in productsForList"
-        :key="item.id"
-        :data="item"
-      />
-    </div>
-    <Paginator
-      :pagination-size="paginationSize"
-      @paginationChange="updateValue"
-    />
-    <!-- <div class="pagination-container">
+    <div class="pagination-container">
       <button
         class="pagination-prev"
         :class="activeIndex == 0 ? 'pagination-arrow-disabled' : ''"
@@ -34,7 +22,10 @@
         :key="index"
         class="pagination-item"
         :class="index == activeIndex ? 'pagination-active' : ''"
-        @click="activeIndex = index"
+        @click="
+          activeIndex = index;
+          emitter();
+        "
       >
         {{ item }}
       </li>
@@ -62,44 +53,17 @@
       >
         &#62;&#62;
       </button>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import { getProducts } from "../repos/ProductsRepo";
-import Product from "./Product.vue";
-import Paginator from "./Paginator.vue";
-@Component({
-  components: {
-    Product,
-    Paginator,
-  },
-})
+
+@Component
 export default class ProductsList extends Vue {
-  // @Prop() private msg!: string;
-
-  isRed = false;
-  productsData: any = [];
+  @Prop() private paginationSize!: number;
   activeIndex = 0;
-  paginationSize = 0;
-  perPage = 10;
-  created() {
-    this.getData();
-  }
-
-  async getData() {
-    try {
-      const response = await getProducts();
-      this.productsData = response.data.products;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  updateValue(activeIndex: any) {
-    this.activeIndex = activeIndex;
-  }
 
   /**
    * @descriptions Handle with pagination actions to set pagination's active index
@@ -112,48 +76,41 @@ export default class ProductsList extends Vue {
       : action == "next-last"
       ? (this.activeIndex = this.paginationSize - 1)
       : "";
+    this.emitter();
   }
-
-  /**
-   * @description Computed getter for filtered products' list
-   * @return {any[]}: products
-   */
-  get productsForList(): any {
-    const products = this.productsData.slice(
-      this.activeIndex * this.perPage,
-      (this.activeIndex + 1) * this.perPage
-    );
-
-    this.paginationSize = this.productsData.length / this.perPage;
-    return products;
+  emitter() {
+    this.$emit("paginationChange", this.activeIndex);
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.container {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  position: relative;
-  width: 100%;
-  .product + .product {
-    margin-left: 20px;
+.pagination-container {
+  margin-top: 10px;
+  li:hover {
+    color: rgba(0, 0, 0, 0.349);
   }
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  button {
+    border: none;
+    border-radius: 15px;
+  }
+  .pagination-prev {
+  }
+  cursor: pointer;
+  .pagination-active {
+    font-weight: 800;
+    transition: transform 0.3s ease;
+    transform: scale(1.2);
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  .pagination-arrow-disabled {
+    color: rgba(0, 0, 0, 0.159);
+    cursor: not-allowed;
+  }
 }
 </style>
--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -7,20 +7,12 @@ const axiosInstance = axios.create({
 });
 mainApp.$HttpService = axiosInstance;
 
-// {
-//     baseURL: "https://jsonplaceholder.typicode.com/"
-//   }
-
 Vue.prototype.$HttpService = axiosInstance;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 mainApp.$HttpService.interceptors.request.use((config: any) => {
-  const apiUrl = window.location.origin;
   if (config.url.includes("[products]")) {
-    // config.url = config.url.replace("[products]", `test/api/products`);
     config.headers["X-Access-Token"] = "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";
-
-    // config.url = config.url.replace("[products]", `${apiUrl}/api/products`);
   }
   return config;
 });
